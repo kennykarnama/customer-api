91b0ac3cd2f88cc792ae9be2dbabc47b9a840361
kennykarnama <kennykarnama@gmail.com>
2022-10-09 15:29:57 +0700

    handle error to map into corresponding http status code (#6)

---
+++ src/main/java/com/example/customerapi/application/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,108 @@
+package com.example.customerapi.application.rest.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+import javax.validation.ConstraintViolationException;
+
+@Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public static final String TRACE = "trace";
+
+    @Value("${reflectoring.trace:false}")
+    private boolean printStackTrace;
+
+    @Override
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Validation error. Check 'errors' field for details.");
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.unprocessableEntity().body(errorResponse);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleNoSuchElementFoundException(NoSuchElementException itemNotFoundException, WebRequest request) {
+        log.error("Failed to find the requested element", itemNotFoundException);
+        return buildErrorResponse(itemNotFoundException, HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleEmptyResultDao(EmptyResultDataAccessException emptyDaoException, WebRequest request) {
+        log.error("Failed to find the requested element", emptyDaoException);
+        return buildErrorResponse(emptyDaoException, HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleConstraintValidationException(ConstraintViolationException constraintException, WebRequest request) {
+        log.error("validation failed", constraintException);
+        return buildErrorResponse(constraintException, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleAllUncaughtException(Exception exception, WebRequest request) {
+        log.error("Unknown error occurred", exception);
+        return buildErrorResponse(exception, "Unknown error occurred", HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception,
+                                                      HttpStatus httpStatus,
+                                                      WebRequest request) {
+        return buildErrorResponse(exception, exception.getMessage(), httpStatus, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception,
+                                                      String message,
+                                                      HttpStatus httpStatus,
+                                                      WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+        if (printStackTrace && isTraceOn(request)) {
+            errorResponse.setStackTrace(ExceptionUtils.getStackTrace(exception));
+        }
+        return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    private boolean isTraceOn(WebRequest request) {
+        String[] value = request.getParameterValues(TRACE);
+        return Objects.nonNull(value)
+                && value.length > 0
+                && value[0].contentEquals("true");
+    }
+
+    @Override
+    public ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        return buildErrorResponse(ex, status, request);
+    }
+}
